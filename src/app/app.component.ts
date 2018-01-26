fd06c59038fe1f1b63a68883a6e0b2a46ec21833
adriane <adriane@kazap.com.br>
2018-01-26 15:55:01 -0200

    primeiros componentes com angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'jad';
+
+  luke = { name: 'Luke', isJedi: true, temple: 'Coruscant' };
+  leia = { name: 'Leia', isJedi: false};
+  han = { name: 'Han Solo', isJedi: false};
 }
